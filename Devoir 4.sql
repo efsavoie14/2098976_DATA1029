9b072486752b1fa41337c48c6fa891dcf78fe312
efsavoie14 <francoissavoie04@gmail.com>
2024-02-11 13:52:03 -0400

    Update Devoir 4.sql

--- Devoir 4.sql
+++ Devoir 4.sql
@@ -1,3 +1,10 @@
 -- question 1 
-SELECT DISTINCT au_id, au_fname, au_lname
+SELECT DISTINCT au_fname, au_lname
+FROM authors
+WHERE au_id NOT IN (
+    SELECT DISTINCT ta.au_id
+    FROM titleauthor ta
+    JOIN titles t ON ta.title_id = t.title_id
+    JOIN publishers p ON t.pub_id = p.pub_id
+    WHERE p.pub_name = 'Harmattan');
 
