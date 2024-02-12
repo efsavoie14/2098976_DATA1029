6a9ca1919a0f913ff09a33496bb770780f624213
efsavoie14 <francoissavoie04@gmail.com>
2024-02-12 12:42:20 -0400

    Update Devoir 4.sql

--- Devoir 4.sql
+++ Devoir 4.sql
@@ -14,4 +14,24 @@ WHERE p.pub_name = 'Harmattan');
 
 SELECT DISTINCT au_fname, au_lname
 FROM authors
+WHERE au_id NOT IN (
+SELECT DISTINCT ta.au_id
+FROM titleauthor ta
+LEFT JOIN titles t ON ta.title_id = t.title_id
+WHERE t.pub_id NOT IN (
+SELECT pub_id
+FROM publishers
+WHERE pub_name = 'Eyrolles'));
 
+-- question 3
+SELECT DISTINCT au_fname, au_lname
+FROM authors
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titleauthor.title_id = titles.title_id
+WHERE advance > (
+SELECT MAX(advance)
+FROM titles
+WHERE pub_id = (
+SELECT pub_id
+FROM publishers
+WHERE pub_name = 'Harmattan'));
